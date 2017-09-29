7d022fd84afb6347d5e3d865a0583804639cfc15
David Keller <email": null>
2017-09-28 19:03:44 -0700

    Set hard params for user, all works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameteres, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameteres
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :birthdate, :location])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :birthdate, :location])
+  end
 end
