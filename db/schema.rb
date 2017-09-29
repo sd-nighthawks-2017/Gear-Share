95196f5ea6bc7183457242422731deeb7226d369
Daniel Way <email": null>
2017-09-28 21:05:59 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170928213813) do
-
 ActiveRecord::Schema.define(version: 20170928221607) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -28,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170928221607) do
     t.boolean "approved", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,4 +46,5 @@ ActiveRecord::Schema.define(version: 20170928221607) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
