da2624029ab00cd0b3f52e11c758cd52302c553d
Michael McDonald <michaelrmcd@gmail.com>
2017-09-29 16:10:45 -0700

    Install RSpec and make example tests for User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  let(:user) { User.new }
+
+  context "when email is empty" do
+    it "should not be valid" do
+      expect(user.valid?).to be false
+    end
+
+    it "should not save" do
+      expect(user.save).to be true
+    end
+  end
+end
