19ba7b88c4fa595548960da0250a4257ca0dfdd1
Daniel Way <email": null>
2017-10-01 11:21:23 -0700

    update routes and styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+    def after_sign_in_path_for(resource)
+      profile_path(current_user.id)
+    end
+
   def configure_permitted_parameteres
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :birthdate, :location])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :birthdate, :location])
