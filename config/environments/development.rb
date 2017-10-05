03b7c40a960fa3d6951af3babf24e790b0ab176c
Daniel Way <email": null>
2017-10-05 12:09:36 -0700

    setup s3 prep to deply

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,18 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Paperclip config:
+config.paperclip_defaults ={
+  storage: :s3,
+  s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+
+  s3_credentials:{
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION')
+  }
+}
+
 Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
 Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
