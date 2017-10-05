603183221d54a6b4909b59c846c0505fd5505261
David Keller <email": null>
2017-10-05 10:29:19 -0700

    Geocode feature functions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @user = @item.user_id
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  
+
   has_many :reviews
   has_many :tasks
 
@@ -16,8 +16,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :location, presence: true
   geocoded_by :location
   after_validation :geocode
+
 
   def average_rating
     if reviews.count == 0
