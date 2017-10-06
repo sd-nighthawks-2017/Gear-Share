2d08351663d82d4b9cb098bf1c9378d50379f86a
Daniel Way <email": null>
2017-10-05 22:55:57 -0700

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_setting = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV.fetch("SENDMAIL_USERNAME"),
-    :password             => ENV.fetch("SENDMAIL_PASSWORD"),
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+     address:              "smtp.gmail.com",
+     port:                 587,
+     user_name:            ENV.fetch("SENDMAIL_USERNAME"),
+     password:             ENV.fetch("SENDMAIL_PASSWORD"),
+    authentication:        "plain",
+    enable_starttls_auto:  true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
